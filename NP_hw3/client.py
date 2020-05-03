ffae91c5f36cc6d4e49bfa633b873b81290b0565
tim310579 <43834434+tim310579@users.noreply.github.com>
2020-05-03 17:07:35 +0800

    Add files via upload

---
+++ NP_hw3/client.py
@@ -0,0 +1,44 @@
+import boto3
+import socket, sys
+
+from sys import argv
+prefix = '0616027hw3-16567629137-10923492449-'
+cli, ip, port = argv
+
+port = int(port)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect((ip, port))
+
+recv = client.recv(1024)
+print(recv.decode(), end = '')
+#recv = client.recv(1024)
+#print(recv.decode(), end = '')
+
+s3 = boto3.resource('s3')
+while True:
+    
+    msg = input('% ')
+    if msg == '':
+        client.send("nothing to do".encode())
+        
+    else:
+        client.send(msg.encode())
+        recv = client.recv(1024)
+        recv = recv.decode()
+        if recv == 'close':
+            break
+        print(recv, end = '')
+        
+        #print(recv == 'Register successfully.\n')
+        if recv == "Register successfully.\n":
+            #print("to herer")
+            #recv = client.recv(1024)
+            #recv = recv.decode()
+            name = msg.split(' ')
+            tmp = prefix + name[1].lower()
+            s3.create_bucket(Bucket=tmp)
+        
+client.close()
+print('Bye!!!!!!')
